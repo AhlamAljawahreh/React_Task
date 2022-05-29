25eea857cc269d7d749f70267a846d264cf3af2c
AhlamAljawahreh <ahlam.aljawahreh@gmail.com>
2022-05-30 01:41:37 +0300

    delete item functionality

--- backend/controllers/Item.js
+++ backend/controllers/Item.js
@@ -20,7 +20,59 @@ const createNewItem = async (req, res) => {
     });
   });
 };
+const getAllItemsByStoreID = (req, res) => {
+
+  let data = req.params.id;
+
+  const query = `SELECT * FROM item WHERE is_deleted=0 AND store_id=? `;
+
+  connection.query(query,data, (err, results) => {
+    if (err) {
+      return res.status(500).json({
+        success: false,
+        massage: "server error*",
+        err: err,
+      });
+    }
+    // result are the data returned by mysql server
+    res.status(200).json({
+      success: true,
+      massage: "All Items in store",
+      results: results,
+    });
+  });
+};
+const deleteItemById = (req, res) => {
+
+  const query = `UPDATE item SET is_deleted = 1   WHERE  id = ?`;
+  const data = req.params.id;
+  connection.query(query, data, (err, results) => {
+    if (err) {
+      return res.status(500).json({
+        success: false,
+        massage: "server error*",
+        err: err,
+      });
+    }
+    // result are the data returned by mysql server
+    if (results.affectedRows != 0) {
+      res.status(201).json({
+        success: true,
+        massage: "item deleted successfully",
+        results: results,
+      });
+    } else {
+      res.status(200).json({
+        success: false,
+        massage: "something happens while deleting item",
+      });
+    }
+  });
+};
+
 
 module.exports = {
   createNewItem,
+  getAllItemsByStoreID,
+  deleteItemById
 };
--- backend/routes/Item.js
+++ backend/routes/Item.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
 //controllers
-const { createNewItem } = require("../controllers/Item");
+const { createNewItem, getAllItemsByStoreID, deleteItemById } = require("../controllers/Item");
 
 //Middleware
 const authentication = require("../middleware/authentication");
@@ -10,4 +10,13 @@ const authorization = require("../middleware/authorization");
 const itemRouter = express.Router();
 
 itemRouter.post("/", authentication, authorization("ADD_ITEM"), createNewItem);
+itemRouter.get("/:id", authentication, getAllItemsByStoreID);
+itemRouter.put(
+  "/:id",
+  authentication,
+  authorization("REMOVE_ITEM"),
+  deleteItemById
+);
+
+
 module.exports = itemRouter;
