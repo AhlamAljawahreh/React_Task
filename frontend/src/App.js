016e4963740eee121d92b53f4bbd89604c380796
AhlamAljawahreh <ahlam.aljawahreh@gmail.com>
2022-05-31 00:39:57 +0300

    routes and route in app component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,22 @@
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import Login from "./components/Login";
+import Navigation from "./components/Navigation";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Start project 5</h1>
+    <div className="App ">
+      <Navigation/>
+      <div className="Home">
+        <Routes>
+          <Route path="/login" element={<Login/>} />
+        </Routes>
+      </div>
     </div>
   );
 }
