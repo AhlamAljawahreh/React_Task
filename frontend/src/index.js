18e1244fe05956012569a55bf111e11debf4a4e4
AhlamAljawahreh <ahlam.aljawahreh@gmail.com>
2022-05-31 00:40:30 +0300

    wrap app component with store and browser route

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Provider } from "react-redux";
+import store from "./Reducers/store";
+import { BrowserRouter } from "react-router-dom";
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>,
+  document.getElementById("root")
 );
 
 
